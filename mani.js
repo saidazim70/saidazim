abc7a6891e1e9be0152d414463ff0851b01415bf
saidazim70 <saidazim1000@gmail.com>
2025-06-02 12:18:47 +0500

    Add files via upload

---
+++ mani.js
@@ -0,0 +1,16 @@
+function salomBer(ism) {
+  console.log("Salom, " + ism + "!");
+}
+
+salomBer("Ali"); // Natija: Salom, Ali!
+salomBer("Laylo"); // Natija: Salom, Laylo!
+let yosh = 18;
+
+if (yosh >= 18) {
+  console.log("Siz katta yoshdasiz");
+} else {
+  console.log("Siz hali voyaga yetmagansiz");
+} 
+for (let i = 1; i <= 5; i++) {
+  console.log("Soni: " + i);
+}
